0f4abc153c4210fbe10c125dbfa40a4704b25099
Maryia Piarshai <mary.pershay@gmail.com>
2022-10-13 10:18:03 +0300

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof (sampleActivity) !== 'string') {
+    return false;
+  }
+
+  if (isNaN(+sampleActivity)) {
+    return false;
+  }
+  else {
+    let rez = Math.ceil((HALF_LIFE_PERIOD * Math.log(MODERN_ACTIVITY / (+sampleActivity))) / 0.693);
+    if (rez == Infinity || rez == -Infinity || isNaN(rez) || rez < 0) return false;
+    return rez;
+  }
 }
 
 module.exports = {
